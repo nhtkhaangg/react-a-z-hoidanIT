3cd34e47f9ddafd65614c22e50124b1f5734d6ad
Nhat Khang <trannhatkhang99@gmail.com>
2025-05-24 21:17:07 +0700

    video-31-cach su dung bien so JSX

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -3,11 +3,23 @@
 
 import './style.css';
 const MyComponent = () => {
+  // const hoidanit = "khaang"; //string
+  // const hoidanit = 25; //number
+  // const hoidanit = true; //boolean
+  // const hoidanit = undefined;
+  // const hoidanit = null;
+  // const hoidanit = [1, 2, 3];
+  // const hoidanit = {
+  //   name: "hoidanit",
+  //   age: 25
+  // };
+  
   return (
 
     <>
-    <div> Khaang </ div>
-    <div className="child" style={ {borderRadius: "10px"} }>child</div>
+    <div> {JSON.stringify(hoidanit)} Khaang </ div>
+    <div> {console.log("ERIC")} </div>
+    <div className="child" style={{borderRadius: "10px"}}>child</div>
     </>
 
   );
