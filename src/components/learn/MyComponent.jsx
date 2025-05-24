c8c88a55dedb94b81870934ead0d13886caaf0dd
Nhat Khang <trannhatkhang99@gmail.com>
2025-05-24 20:37:19 +0700

    video-30-JSX

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,7 +1,17 @@
+// JSX: 1 parent
+// Fragment
+
+import './style.css';
 const MyComponent = () => {
   return (
-    <div> Khaang </div>
+
+    <>
+    <div> Khaang </ div>
+    <div className="child" style={ {borderRadius: "10px"} }>child</div>
+    </>
+
   );
+  
 };
 
 export default MyComponent
