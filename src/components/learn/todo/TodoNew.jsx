af518dde136c417d7e177adc1c64399b283e6e31
Nhat Khang <trannhatkhang99@gmail.com>
2025-05-25 21:59:21 +0700

    video-36 Dom Event

--- src/components/learn/todo/TodoNew.jsx
+++ src/components/learn/todo/TodoNew.jsx
@@ -1,12 +1,25 @@
 const TodoNew = (props) => {
+  console.log(">>>check point: ", props);
+  const { addNewTodo } = props;
+  // addNewTodo("Khaang");
 
-    console.log (">>>check point: ", props)
-const {addNewTodo} = props;
-// addNewTodo("Khaang");
+  const handleClick = () => {
+    alert("Click me");
+  };
+  const handleOnChange = (name) => {
+    console.log(">>>hanleOnChange", name)
+  }
+
   return (
     <div className="toto-new">
-      <input type="text" />
-      <button>Add</button>
+      <input type="text" 
+      onChange={(event) => {handleOnChange(event.target.value)}}
+      />
+
+      <button style={{ cursor: "pointer" }}
+      onClick ={handleClick}
+      >Add</button>
+
     </div>
   );
 };
